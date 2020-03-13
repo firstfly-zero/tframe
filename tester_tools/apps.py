768f3f2be51069864b0b7246f53d3dfdf959972f
jiayifei <jiayifei@haodf.com>
2020-03-14 01:21:09 +0800

    tester知识框架从零到一

---
+++ tester_tools/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TesterToolsConfig(AppConfig):
+    name = 'tester_tools'
---
+++ tester_tools/views/case_design.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+"""
+辅助于用例设计的工具：
+1.等价类划分
+2.边界值
+3.因果图
+4.场景法（冒烟测试）
+5.流程法
+6.错误推断法（倚靠经验，需要对过去漏测的bug进行总结提炼）
+7.正交表用例设计
+"""
+# Create your views here.
+
+
